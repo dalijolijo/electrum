92504023f7211a294a2e85aec659489e72ff0459
David <37987466+dalijolijo@users.noreply.github.com>
2019-03-18 12:38:09 +0100

    skip diff check for now too

--- electrum/blockchain.py
+++ electrum/blockchain.py
@@ -172,9 +172,9 @@ class Blockchain(util.PrintError):
             raise Exception("prev hash mismatch: %s vs %s" % (prev_hash, header.get('prev_block_hash')))
         if constants.net.TESTNET:
             return
-        bits = self.target_to_bits(target)
-        if bits != header.get('bits'):
-            raise Exception("bits mismatch: %s vs %s" % (bits, header.get('bits')))
+        #bits = self.target_to_bits(target)
+        #if bits != header.get('bits'):
+        #    raise Exception("bits mismatch: %s vs %s" % (bits, header.get('bits')))
         if int('0x' + _hash, 16) > target:
             raise Exception("insufficient proof of work: %s vs target %s" % (int('0x' + _hash, 16), target))
 
